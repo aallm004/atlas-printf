443af02089ca44e5fd200699f9a05ac23076f288
Jeremy Mitts <jeremy.mitts@atlasschool.com>
2024-02-29 23:17:06 +0000

    main.h initial push

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef (struct op
+{
+	char *op;
+	char *(f)(const char *format, int i);
+}) op_t;
+
+int _strlen(char *);
+int _printf(const char *format, ...);
+
+#endif
