c8bbea225e840a7cd776b1c301fbb70b8fbae1ff
jerm014 <jermitts@gmail.com>
2024-03-01 02:15:00 +0000

    added functions file rev 0

---
+++ functions.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * op_char - a function to return a character from a valist
+ *
+ * @ap:      the valist
+ *
+ * Returns:  a string array of one character
+ *
+ */
+
+char *op_char(va_list ap)
+{
+        return(va_arg(ap, char));
+}
+
+/**
+ * op_string- a function to return a string from a valist
+ *
+ * @ap:       the va list
+ *
+ * Return:    a string array of characters
+ *
+ */
+
+char *op_string(valist ap)
+{
+        return(va_arg(ap, char *));
+}
+
+/**
+ * op_percent- a placeholder function to return a percent
+ *
+ * @ap:        the va list
+ *
+ * Return:     a string array of one %
+ *
+ */
+
+char *op_percent(va_list ap)
+{
+        return("%");
+}
+
